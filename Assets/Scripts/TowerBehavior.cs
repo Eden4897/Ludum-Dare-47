532c4eaff2c5f676d1a9e96cd51187f8dc758b41
Eden <edencheung5112006@gmail.com>
2020-10-03 18:14:37 +0800

    Added tower shooting and recoding

--- Assets/Scripts/TowerBehavior.cs
+++ Assets/Scripts/TowerBehavior.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TowerBehavior : MonoBehaviour
 {
     protected Camera cam;
 
+    //bullet shooting calculations
     private float _timeSinceLastShot = 0;
-    protected float timeSinceSpawn = 0;
-    protected float reloadInterval = 0.2f;
+    protected float reloadInterval = 1;
 
-    protected Dictionary<float, float> recording = new Dictionary<float, float>();
+    //bullet info
+    protected float bulletSpeed = 5;
+    protected float bulletLife = 1;
 
+    //behavior
     private float degrees;
+    protected float timeSinceSpawn = 0;
+    protected float recordingTimeFrame = 10;
+
+    protected bool isControlled = true;
+    protected List<KeyValuePair<float, float>> recording = new List<KeyValuePair<float, float>>();
 
     [SerializeField] private GameObject Bullet;
 
@@ -23,14 +32,35 @@ public class TowerBehavior : MonoBehaviour
 
     private void Update()
     {
-        timeSinceSpawn += Time.deltaTime;
-        FollowMouse(cam.ScreenToWorldPoint(Input.mousePosition));
-
-        _timeSinceLastShot += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0) && _timeSinceLastShot >= reloadInterval)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            _timeSinceLastShot = 0;
-            Shoot();
+            LoseControl();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GainControl();
+        }
+        timeSinceSpawn += Time.deltaTime;
+
+        if (isControlled)
+        {
+            FollowMouse(cam.ScreenToWorldPoint(Input.mousePosition));
+
+            _timeSinceLastShot += Time.deltaTime;
+            if (Input.GetMouseButtonDown(0) && _timeSinceLastShot >= reloadInterval)
+            {
+                _timeSinceLastShot = 0;
+                Shoot();
+                recording.Add(new KeyValuePair<float, float>(timeSinceSpawn, -degrees));
+                if(recording[0].Key <= timeSinceSpawn - 10)
+                {
+                    recording.RemoveAt(0);
+                }
+            }
+        }
+        else
+        {
+
         }
     }
 
@@ -42,9 +72,54 @@ public class TowerBehavior : MonoBehaviour
 
     protected virtual void Shoot()
     {
-        GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.Euler(0, 0, degrees), transform);
-        
-        recording.Add(timeSinceSpawn, degrees);
-        
+        Vector2 direction = Quaternion.Euler(0f,0f,90f) * new Vector2(Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad) , Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad));
+
+        GameObject newBullet = Instantiate(Bullet, (Vector2)transform.position + direction * 0.5f, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+        Utility.Invoke(()=> 
+        {
+            newBullet.GetComponent<Bullet>().OnCollide(); 
+        }, 
+        bulletLife);
+    }
+
+    protected virtual void LoseControl()
+    {
+        isControlled = false;
+        //clean recording
+
+        float startTime = recording[0].Key;
+        List<KeyValuePair<float, float>> newRecording = new List<KeyValuePair<float, float>>();
+        foreach(var action in recording)
+        {
+            newRecording.Add(new KeyValuePair<float, float>(action.Key - startTime, action.Value));
+        }
+
+        StartCoroutine(Play());
+    }
+
+    protected virtual void GainControl()
+    {
+        isControlled = false;
+        StopCoroutine(Play());
+    }
+
+    protected virtual IEnumerator Play()
+    {
+        while (true)
+        {
+            float _t = 0;
+            for(int i = 0; i < recording.Count; ++i)
+            {
+                while (_t <= recording[i].Key)
+                {
+                    _t += Time.deltaTime;
+                    yield return null;
+                }
+                transform.eulerAngles = new Vector3(0f, 0f, recording[i].Value);
+                Shoot();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Utility : MonoBehaviour
+{
+    public static Utility instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static void Invoke(Action target, float timeout)
+    {
+        instance.StartCoroutine(instance.InvokeRoutine(target, timeout));
+    } 
+
+    private IEnumerator InvokeRoutine(Action target, float timeout)
+    {
+        yield return new WaitForSecondsRealtime(timeout);
+        target();
+    }
+}
