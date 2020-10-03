94002b05f82cb3432280b53b69a811534b77844d
Eden <edencheung5112006@gmail.com>
2020-10-03 23:03:51 +0800

    Enemy pathfinding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public List<Vector2> path = new List<Vector2>();
+    private float speed = 10;
+    private float _t = 0;
+
+    public IEnumerator Move()
+    {
+        for (int i = 1; i < path.Count; ++i)
+        {
+            Vector2 start = path[i - 1];
+            Vector2 end = path[i];
+            _t = 0;
+            float distance = Vector2.Distance(start, end);
+            while ((Vector2)transform.position != end)
+            {
+                _t += Time.deltaTime;
+                transform.position = Vector2.Lerp(start, end, _t/ distance * speed);
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private List<Vector2> path;
+
+    private List<float> spawns;
+
+    [SerializeField] private GameObject enemy;
+
+    public List<Enemy> enemies;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject newObj = Instantiate(enemy);
+            Enemy newEnemy = newObj.GetComponent<Enemy>();
+            newEnemy.path = path;
+            StartCoroutine(newEnemy.Move());
+            enemies.Add(newEnemy);
+        }
+    }
+}
