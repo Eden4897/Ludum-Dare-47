67eb49be2bf836e3ed2bad46fe8736d126aef0c4
scsc <scscgit@users.noreply.github.com>
2020-10-04 03:37:32 +0200

    Enemy status effects as parameter multipliers and color modifiers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,31 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Enemy : MonoBehaviour
 {
-    public int damageToPlayer = 10;
+    public float initialSpeed = 1;
+    public float initialHealth = 1;
+    public float initialDamageToPlayer = 10;
+    public List<EnemyStatus> statuses = new List<EnemyStatus>();
+    public float health;
 
     private Rigidbody2D _rigidbody;
     private Collider2D _collider;
+    private SpriteRenderer _spriteRenderer;
+
+    public float Speed =>
+        statuses.Aggregate(initialSpeed, (acc, modifier) => acc * modifier.speedMultiplier);
+
+    public float MaxHealth =>
+        statuses.Aggregate(initialHealth, (acc, modifier) => acc * modifier.healthMultiplier);
+
+    public float DamageToPlayer =>
+        statuses.Aggregate(initialDamageToPlayer, (acc, modifier) => acc * modifier.damageMultiplier);
+
+    public Color Color =>
+        statuses.Aggregate(_spriteRenderer.color, (acc, modifier) => acc * modifier.colorEffect);
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _collider = GetComponent<Collider2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         Assert.IsTrue(_rigidbody.isKinematic);
         Assert.IsTrue(_collider.isTrigger);
     }
 
+    private void Start()
+    {
+        if (Random.Range(1, 10) > 9)
+        {
+            statuses.Add(EnemyStatus.Virus);
+        }
+
+        if (Random.Range(1, 10) > 8)
+        {
+            statuses.Add(EnemyStatus.Tank);
+        }
+
+        if (Random.Range(1, 10) > 7)
+        {
+            statuses.Add(EnemyStatus.Jackrabbit);
+        }
+
+        health = MaxHealth;
+        _spriteRenderer.color = Color;
+    }
+
     public List<Vector2> path = new List<Vector2>();
     private float _t = 0;
-
-    protected float speed = 1f;
-
-    protected float health = 1f;
 
     public IEnumerator Move()
     {
@@ -38,7 +75,7 @@ public class Enemy : MonoBehaviour
             while ((Vector2) transform.position != end)
             {
                 _t += Time.deltaTime;
-                transform.position = Vector2.Lerp(start, end, _t / distance * speed);
+                transform.position = Vector2.Lerp(start, end, _t / distance * Speed);
                 yield return null;
             }
         }
@@ -56,7 +93,7 @@ public class Enemy : MonoBehaviour
 
     public void OnFinish()
     {
-        GameManager.Instance.Damage(damageToPlayer);
+        GameManager.Instance.Damage((int) DamageToPlayer);
         EnemyManager.Instance.Despawn(this);
     }
 }
---
+++ Assets/Scripts/EnemyStatus.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public struct EnemyStatus
+{
+    public static readonly EnemyStatus Jackrabbit = new EnemyStatus
+    {
+        speedMultiplier = 5f,
+        healthMultiplier = 1f,
+        damageMultiplier = 1f,
+        colorEffect = new Color(1, 0.62f, 1, 0.76f),
+    };
+
+    public static readonly EnemyStatus Tank = new EnemyStatus
+    {
+        speedMultiplier = 0.5f,
+        healthMultiplier = 5f,
+        damageMultiplier = 1f,
+        colorEffect = new Color(0, 0.62f, 0.82f, 1),
+    };
+
+    public static readonly EnemyStatus Virus = new EnemyStatus
+    {
+        speedMultiplier = 1.4f,
+        healthMultiplier = 0.8f,
+        damageMultiplier = 6f,
+        colorEffect = Color.green,
+    };
+
+    public static readonly EnemyStatus Default = new EnemyStatus
+    {
+        speedMultiplier = 1f,
+        healthMultiplier = 1f,
+        damageMultiplier = 1f,
+        colorEffect = Color.white,
+    };
+
+    public float speedMultiplier;
+    public float healthMultiplier;
+    public float damageMultiplier;
+    public Color colorEffect;
+}
